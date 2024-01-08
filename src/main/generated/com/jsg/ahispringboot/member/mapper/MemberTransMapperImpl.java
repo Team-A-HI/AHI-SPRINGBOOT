d561ed3d9bf2932fa997b1411c56bcbb18c3ea10
JH <145424127+kioioy@users.noreply.github.com>
2024-01-08 16:48:35 +0900

    commit

--- src/main/generated/com/jsg/ahispringboot/member/mapper/MemberTransMapperImpl.java
+++ src/main/generated/com/jsg/ahispringboot/member/mapper/MemberTransMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-08T16:41:09+0900",
+    date = "2024-01-08T16:47:18+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.5.jar, environment: Java 17.0.9 (Oracle Corporation)"
 )
 public class MemberTransMapperImpl implements MemberTransMapper {
